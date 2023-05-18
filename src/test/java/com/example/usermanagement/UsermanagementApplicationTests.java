81f6fb7ca938404c34bac0039966fc77723f15e9
Paris Malakounidis <paris.malakounidis@oddschecker.com>
2023-05-18 16:04:32 +0300

    component test

--- src/test/java/com/example/usermanagement/UsermanagementApplicationTests.java
+++ src/test/java/com/example/usermanagement/UsermanagementApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.usermanagement;
 
+import com.example.usermanagement.dto.UserDto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UsermanagementApplicationTests {
 
+@Autowired
+	private TestRestTemplate testRestTemplate;
 	@Test
-	void contextLoads() {
+	public void createUser() {
+		UserDto userDto = new UserDto();
+		userDto.setFirstName("parikios");
+		userDto.setEmail("a@a.a");
+		userDto.setLastname("gfdgfdgfd");
+		HttpEntity<UserDto> httpEntity = new HttpEntity<>(userDto);
+		ResponseEntity<UserDto> responseEntity = testRestTemplate.exchange("/user", HttpMethod.POST, httpEntity, UserDto.class);
+		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+		UserDto returnedUserDto = responseEntity.getBody();
+		assertEquals("a@a4.a", returnedUserDto.getEmail());
 	}
 
 }
